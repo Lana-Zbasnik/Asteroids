2e4d457958f6e1706d92c1d3069e8e80d447f9a2
Lana-Zbasnik <115007072+Lana-Zbasnik@users.noreply.github.com>
2022-10-04 20:29:09 -0600

    Add files via upload

---
+++ Bullet.java
@@ -0,0 +1,47 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Bullet extends Circle {
+	
+	  
+	private static final int RADIUS = 5;
+	private double rotation;
+
+
+	public Bullet(Point center, double rotation) {
+		super(center, RADIUS); // define RADIUS in Bullet class
+		this.rotation = rotation;
+		}
+
+
+	@Override
+	public void paint(Graphics brush, Color color) {
+		brush.setColor(color);
+		brush.fillOval((int) center.x, (int) center.y, radius, radius);
+		
+	}
+
+	@Override
+	public void move() {
+		center.x += 2 * Math.cos(Math.toRadians(rotation));
+		center.y += 2 * Math.sin(Math.toRadians(rotation));
+	
+	}
+
+	public Point getCenter() {
+	
+	return center;
+	
+	}
+	public boolean outOfBounds() {
+		
+		if ( center.x > Asteroids.SCREEN_WIDTH || center.x < 0 || center.y > Asteroids.SCREEN_HEIGHT || center.y < 0) {
+			
+			return true;
+			
+		}  
+		return false;
+	}
+	
+
+	}
